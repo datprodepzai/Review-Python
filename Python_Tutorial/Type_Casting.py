ae65a3b9a507394722c785df405f2631951c72e9
Pham Tien Dat <phamdatha@gmail.com>
2024-12-08 13:55:34 +0700

    Learn Type Casting(ép kiểu) in python

---
+++ Python_Tutorial/Type_Casting.py
@@ -0,0 +1,22 @@
+name = ""
+age = 22
+gpa = 3.4
+Is_Student = True
+
+# print(type(Is_Student))
+#=======================================
+# print(type(gpa))
+# gpa = int(gpa)
+# print(type(gpa))
+# print(gpa)
+#=======================================
+# age = str(age)
+# print(type(age))
+# print(age)
+#
+# age += '1'
+# print(age)
+#=======================================
+name = bool(name)
+print(name)
+# We could use this to check to see if somebody enters in their name or not
