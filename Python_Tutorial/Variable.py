a7489afedbfa02b74c76d1ebfdf11b463235b198
Pham Tien Dat <phamdatha@gmail.com>
2024-12-08 13:27:50 +0700

    Learn Variable in python

---
+++ Python_Tutorial/Variable.py
@@ -0,0 +1,40 @@
+
+# string
+first_name = "Dat"
+food = "Chicken_Rice"
+email = "phamdatha@gmail.com"
+
+# print(f"Hello{first_name}")
+# print(f"You can like {food}")
+# print(f"You use {email}")
+
+# Interge
+age = 22
+quantity = 3
+Num_Of_Student = 55
+
+# print(f"You are {age} year old")
+# print(f"You are buying {quantity} items")
+# print(f"Your class has {Num_Of_Student} students")
+
+# float
+price = 7.99
+gpa = 3.4
+distance = 5.3
+
+# print(f"The price is: {price}$")
+# print(f"My gpa is: {gpa}")
+# print((f"The distance from home to my school is {distance}km"))
+
+# Boolean
+Is_Student = False
+For_Sale = False
+
+# if Is_Student:
+#     print("You are a Student")
+# else: print("You aren't a Student")
+
+if For_Sale:
+    print("That item is for sale")
+else:
+    print("That item is NOT avalable")
