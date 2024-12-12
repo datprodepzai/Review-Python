8d0b8d58d210ea9e1b129d620fdbed0ee3a624f8
Pham Tien Dat <phamdatha@gmail.com>
2024-12-12 16:57:06 +0700

    Accepct User input

---
+++ Excercise/Rectangle.py
@@ -0,0 +1,7 @@
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+
+#you can print ²  by :  alt + 0178
+print(f"The area is: {area} cm²")
+
---
+++ Python_Tutorial/User_input.py
@@ -0,0 +1,8 @@
+name = input("what your name bro: ")
+age = int(input("How old are you: "))
+
+age = age + 1
+
+print(f"Hey {name} !")
+print("Happy birthday")
+print(f"You are {age} years old")
