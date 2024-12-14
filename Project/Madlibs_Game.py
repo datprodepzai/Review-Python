255dc3d440f25f210d51292729f917e662439315
Pham Tien Dat <phamdatha@gmail.com>
2024-12-14 19:51:28 +0700

    Word game where you creat a story (vietnamese)

--- Project/Madlibs_Game.py
+++ Project/Madlibs_Game.py
@@ -2,4 +2,15 @@
 # Word game where you creat a story
 # by filling in blanks with random words
 
-print(f"Today I went to a {adjective1} zoo.")
+adjective1 = input("Nhập vào tính từ  (Mô tả): ")
+noun1 = input("Nhập vào danh từ (person, place, thing): ")
+
+adjective2 = input("Nhập vào tính từ  (Mô tả): ")
+verb1 = input("Nhập vào moột hành động : ")
+adjective3 = input("Nhập vào tính từ  (Mô tả): ")
+
+
+print(f"Hôm nay tôi đi sở thú")
+print(f"Ở trong đó tôi nhìn thấy {noun1}")
+print(f"{noun1} với sự {adjective2} và {verb1}")
+print(f"Tôi cảm thấy rất {adjective3}!")
