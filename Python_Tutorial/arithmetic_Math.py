adb5deda2a3490c0a5b4e4ed9c046d824a73f1a5
Pham Tien Dat <phamdatha@gmail.com>
2024-12-15 07:55:57 +0700

    Math and Arithmetic

---
+++ Python_Tutorial/arithmetic_Math.py
@@ -0,0 +1,63 @@
+import math
+
+radius = float(input("Enter the radius of a Circle: "))
+
+circumference = 2 * math.pi * radius
+area = math.pi * pow(radius,2)
+
+print(f"The circumference is: {round(circumference, 2)} cm")
+print(f"The area is: {round(area, 2)}")
+
+
+
+
+
+
+
+#==========================================================
+# print(math.pi)
+# print(math.e)
+
+# x = 121.9
+# # result = math.sqrt(x)
+# # result = math.ceil(x) # Làm tròn lên
+# # result = math.floor(x) # Làm tròn xuống
+#
+#
+# print(result)
+
+
+#==========================================================
+# x = 3.14
+# y = -4
+# z = 5
+#
+# # result = round(x)  # Làm tròn
+# # result = abs(y) # trị tuyệt đối
+# # result = pow(4, 2) # 4²
+# # result = max(x, y, z)
+# # result = min(x, y, z)
+#
+# print(result)
+
+
+#==========================================================
+# friends = 5
+#
+# #friends = friends + 1
+# #friends += 1
+#
+# #friends = friends - 2
+# #friends -= 2
+#
+# # friends = friends * 3
+# # friends *= 3
+#
+# # friends /= 2
+#
+# # friends = friends ** 2
+# # friends **= 2
+#
+#
+#
+# print(friends)
