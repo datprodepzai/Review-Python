7c1ac56993a81fb3b1ad2fb4202fcf680c0de3af
Pham Tien Dat <phamdatha@gmail.com>
2024-12-15 12:07:28 +0700

    Nhặt các kí tự trong chuỗi

---
+++ Python_Tutorial/string_indexing.py
@@ -0,0 +1,13 @@
+credit_number = "1234-5678-88"
+
+# print(credit_number[0])
+
+# print(credit_number[0:8])
+
+# print(credit_number[:5])
+
+# print(credit_number[5:])
+
+# print(credit_number[-1])
+
+print(credit_number[::4]) # Lấy các phần tử với bước nhảy là 4
