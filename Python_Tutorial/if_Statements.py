ab7beef94db33b182cccfff27eacb59ff66f3b02
Pham Tien Dat <phamdatha@gmail.com>
2024-12-15 08:30:05 +0700

    Math and Arithmetic

---
+++ Python_Tutorial/if_Statements.py
@@ -0,0 +1,31 @@
+# age = int(input("Enter your age: "))
+#
+# if age >= 18:
+#     print("You are signed up!")
+# elif age < 0:
+#     print("You haven't been born yet !")
+# elif age >= 100:
+#     print("You are too old to sign up")
+# else:
+#     print("You must be 18+ to sign up")
+# =========================================================
+
+# response = input("Would you like food? (Y/N): ")
+# if response == 'Y':
+#     print("Have some food ")
+# else:print("No food for you !!")
+# =========================================================
+
+# name = input("Enter your name: ")
+# if name == "":
+#     print("You did not type in your name !!")
+# else : print(f"hello {name}")
+
+# =========================================================
+
+for_Sale = True
+
+if for_Sale:
+    print("This item is for sale !")
+else:
+    print("This item not for sale !")
