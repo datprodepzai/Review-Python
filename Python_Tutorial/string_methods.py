bd62bdd9d3af83cbb2e7209e105a12e07c6586e8
Pham Tien Dat <phamdatha@gmail.com>
2024-12-15 11:55:24 +0700

    String method

---
+++ Python_Tutorial/string_methods.py
@@ -0,0 +1,32 @@
+name = input("enter your full name: ")
+#
+# count =  len(name)
+#
+# char = "D"
+#
+# result = name.rfind(char)
+#
+# in_Hoa_Chu_first = name.capitalize()
+# in_Hoa_All = name.upper()
+# viet_thuong = name.lower()
+# ktra_string_num = name.isdigit() # trả về true or false , true nếu toàn số
+#
+#
+#
+# print(f"số kí tự {count}")
+# print(f"{char} ở vị trí {result}")
+#
+# print(in_Hoa_Chu_first)
+# print(in_Hoa_All)
+# print(viet_thuong)
+# print(ktra_string_num)
+
+# name = name.replace(" ", "-")
+# print(name)
+
+# print(help(str))
+
+print(name.find(" ")) # trả về -1 nếu không tìm thấy
+
+print(name.isalpha()) # true nếu không có số , false nếu có số
+
